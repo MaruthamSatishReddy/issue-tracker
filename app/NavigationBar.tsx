65bff1e7a82561a95df47b201c4732068df2a9a2
satishlakshmiudemy@gmail.com <satishlakshmiudemy@gmail.com>
2023-10-12 19:36:53 +0530

    Create Issue Model

--- app/NavigationBar.tsx
+++ app/NavigationBar.tsx
@@ -11,14 +11,15 @@ const NavigationBar = () => {
     const links = [{ label: 'DashBoard', href: '/' }, { label: 'Issues', href: '/Issues' }]
 
     return (
-        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'><Link href="/"><AiFillBug></AiFillBug></Link>
+        <nav className='flex space-x-6 border-b mb-5 px-5 h-20 items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xl'><Link href="/" className='color'><AiFillBug size={40} /></Link>
             <ul className='flex space-x-6'>
                 {links.map(link => <Link key={link.href}
                     href={link.href}
                     className={classNames({
-                        'text-zinc-900': link.href === currentPath,
-                        'text-zinc-400': link.href !== currentPath,
-                        'hover:text-zinc-800 transition-colors': true
+                        'text-white': link.href === currentPath,
+                        'text-gray-400': link.href !== currentPath,
+                        'hover:text-gray-800 transition-colors': true,
+                        'font-semibold': true
                     })}>{link.label}</Link>)}
 
             </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-lg font-semibold text-gray-400 text-center'>Issue Tracker ..Coming Soon</div>
+    <div className='text-lg font-extrabold text-blue-700 text-center '>Issue Tracker ..Coming Soon</div>
   )
 }
