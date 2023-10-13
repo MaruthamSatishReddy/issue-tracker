a0e8d90c959c1f28eb752d36cb85f6cee51c241e
satishlakshmiudemy@gmail.com <satishlakshmiudemy@gmail.com>
2023-10-13 11:38:43 +0530

    Create Issue

--- app/api/Issues/route.ts
+++ app/api/Issues/route.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import prisma from "@prisma/client";
-const createIssueSchema=z.object({ tite: z.string().min(1).max(120) , description: z.string().min(1).max(100) })
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+const createIssueSchema=z.object({ title: z.string().min(1).max(120) , description: z.string().min(1).max(100) })
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    const issueCreateValidation=createIssueSchema.safeParse(body);
+    const issueCreateValidation = createIssueSchema.safeParse(body);
+    
     if (!issueCreateValidation.success) {
         return NextResponse.json(issueCreateValidation.error.errors,{status:400})
     }
+   
+    const createNewIssue = prisma.issue.create({ data: { title: body.title, description: body.description } });
+    return NextResponse.json(createNewIssue,{status:201})
 }
