4dc9592ea1c4f81424eac7538274b8aaf922eee7
satishlakshmiudemy@gmail.com <satishlakshmiudemy@gmail.com>
2023-10-13 11:58:54 +0530

    Issue Create Model

---
+++ app/Issues/new/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import { TextField, TextArea, Button } from "@radix-ui/themes"
+const NewIssuePage = () => {
+    return (
+        <div className='max-w-xl space-y-3'><TextField.Root>
+            <TextField.Input placeholder="Title" />
+        </TextField.Root>
+            <TextArea placeholder="Description" />
+            <Button>Create Issue</Button>
+        </div>
+    )
+}
+
+export default NewIssuePage
--- app/Issues/page.tsx
+++ app/Issues/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Button } from "@radix-ui/themes"
+import Link from 'next/link'
 const IssuesPage = () => {
     return (
-        <div><Button>Create Issue</Button></div>
+        <div><Button><Link href="/Issues/new">New Issue</Link></Button></div>
     )
 }
 
